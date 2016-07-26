baa5bbf6d46b27bb62209c9fa81ab872c61f0462
Eric <eric.stouch@gmail.com>
2016-07-26 08:07:39 -0400

    Corrected comment

--- itemReq.py
+++ itemReq.py
@@ -6,7 +6,7 @@ HEADERS = {"X-API-KEY": '381bc3958d3541bf8db576788bd29d53'}
 #make request for Gjallarhorn
 r = requests.get("https://www.bungie.net/platform/Destiny/Manifest/InventoryItem/1274330687/", headers=HEADERS);
 
-#convert the json objet we received into a Pythong dictionary object and
+#convert the json objet we received into a Python dictionary object and
 #print the name of the item
 inventoryItem = r.json()
 
